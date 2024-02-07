13f6302b35b7808ad307b32b904cdeb7a29a828b
Juanchy Quintana <quintanajuanchy25@gmail.com>
2024-02-07 11:34:04 -0300

    se avanzó con el header

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,26 +1,20 @@
-import { Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import logoCoffe from '../../assets/Coffee_Logo.png'
 
 const Header = () => {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className="bg-body-tertiary shadow">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">
+            <img src={logoCoffe} alt="Logo Coffe" className="img-fluid" width={160} />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto text-center">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+          <Nav className="ms-auto text-center">
+            <Nav.Link href="#inicio">Inicio</Nav.Link>
+            <Nav.Link href="#link">Administrador</Nav.Link>
+            <Nav.Link href="#link">Registro</Nav.Link>
+            <Nav.Link href="#link">Login</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
