14395b783661480949a0ccec11700a62fe2b5d9e
Juanchy Quintana <quintanajuanchy25@gmail.com>
2024-02-09 12:14:47 -0300

    se creó la seccion de los productos en la página principal

--- src/components/CardProducto.jsx
+++ src/components/CardProducto.jsx
@@ -1,7 +1,31 @@
+import { Button, Card } from "react-bootstrap"
 
 const CardProducto = () => {
   return (
-    <div>CardProducto</div>
+    <>
+      <section className="my-5">
+
+        <div className="mb-3">
+          <h2>- Nuestros Productos -</h2>
+        </div>
+
+        <div>
+          <Card className="w-50">
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Café Americano</Card.Title>
+            <Card.Text>
+              Una taza de café suave y aromático
+            </Card.Text>
+          </Card.Body>
+
+          <Card.Footer>
+            <Button variant="success">Ver más</Button>
+          </Card.Footer>
+          </Card>
+        </div>
+      </section>
+    </>
   )
 }
 
