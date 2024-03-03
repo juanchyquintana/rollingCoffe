7f009037a07c56e959a364a0215503b3d4b07974
Juanchy Quintana <quintanajuanchy25@gmail.com>
2024-03-03 20:59:07 -0300

    se agregó los productos en el inicio, luego se agregó una nueva opcion en el nav y se mapearon los productos correctamente

--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -3,10 +3,9 @@ import banner from "../../assets/banner.png";
 import CardProducto from "../CardProducto";
 import { useEffect, useState } from "react";
 import { leerProductosAPI } from "../../helpers/queries";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Inicio = () => {
-
   const [productos, setProductos] = useState([]);
 
   useEffect(() => {
@@ -22,23 +21,35 @@ const Inicio = () => {
   }, []);
   return (
     <section className="mainPage">
-        <img
-          src={banner}
-          alt="Banner de Rolling Coffe"
-          className="banner w-100 block"
-        />
+      <img
+        src={banner}
+        alt="Banner de Rolling Coffe"
+        className="banner w-100 block"
+      />
 
-        <Container>
-          <Row>
-            {productos.slice(0, 3).map( producto => (
-              <CardProducto 
-                key={producto.id}
-                producto={producto}
-              />
-            ))}
-          </Row>
-        </Container>
-      </section>
+      <Container>
+        <div className="d-flex justify-content-between align-items-center categoria">
+          <h3 className="my-4 mt-5 titulosFuente">PRODUCTOS POPULARES</h3>
+          <Link
+            to={"/productos"}
+            className="link-secondary link-informacion mt-5 fw-bold h5"
+          >
+            Otros productos
+          </Link>
+        </div>
+        <Row className="mb-4 mt-5 ms-4">
+          {productos && productos.length > 0 ? (
+            productos
+              .slice(0, 3)
+              .map((producto) => (
+                <CardProducto key={producto.id} producto={producto} />
+              ))
+          ) : (
+            <p className="text-center fw-bold">No hay recetas disponibles</p>
+          )}
+        </Row>
+      </Container>
+    </section>
   );
 };
 
